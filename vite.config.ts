898795f173eb2d3cc8d6b059755e7faf8fa812c3
Sai Aman Pothuganti <saiamanpothuganti@gmail.com>
2024-12-28 21:11:56 +0530

    use ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import ssr from 'vike/plugin'
+import md from 'unplugin-vue-markdown/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/psa-my/",
+  base: '/psa-my/',
   plugins: [
-    vue(),
+    ssr({ prerender: true }),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
     vueJsx(),
     vueDevTools(),
+    md({}),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
