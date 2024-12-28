c4c79a6c56503537a00d370f334c1ee304264476
Sai Aman Pothuganti <saiamanpothuganti@gmail.com>
2024-12-28 21:13:14 +0530

    update dist subtree

---
+++ dist/server/chunks/chunk-1ap2In98.js
@@ -0,0 +1,21 @@
+import { defineComponent, reactive, useSSRContext } from "vue";
+import { ssrRenderAttrs, ssrInterpolate } from "vue/server-renderer";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "Counter",
+  __ssrInlineRender: true,
+  setup(__props) {
+    const state = reactive({ count: 0 });
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<button${ssrRenderAttrs(_attrs)}>Counter ${ssrInterpolate(state.count)}</button>`);
+    };
+  }
+});
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("components/Counter.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as _
+};
