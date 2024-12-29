57afbac4e0a23473f85fbdb0b0d66b2c5527a74d
Sai Aman Pothuganti <saiamanpothuganti@gmail.com>
2024-12-29 15:42:14 +0530

    update dist subtree

--- server/entries/renderer_default-page-server.mjs
+++ server/entries/renderer_default-page-server.mjs
@@ -153,7 +153,7 @@ function getPageTitle(pageContext) {
   const title = (
     // For static titles (defined in the `export { documentProps }` of the page's `.page.js`)
     (pageContext.exports.documentProps || {}).title || // For dynamic tiles (defined in the `export addContextProps()` of the page's `.page.server.js`)
-    (pageContext.documentProps || {}).title || "Demo"
+    (pageContext.documentProps || {}).title || "Psa My"
   );
   return title;
 }
