424af81fcb01e4f3753c2e3e496bc7aeb6a774dd
Sai Aman Pothuganti <saiamanpothuganti@gmail.com>
2024-12-29 11:00:58 +0530

    temp

--- dist/server/chunks/chunk-BChgxANp.js
+++
@@ -1,8 +0,0 @@
-function filterMovieData(movie) {
-  const { id, title, release_date, director, producer } = movie;
-  movie = { id, title, release_date, director, producer };
-  return movie;
-}
-export {
-  filterMovieData as f
-};
