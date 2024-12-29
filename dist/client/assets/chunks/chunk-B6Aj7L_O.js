8264b925ada865ebadd7a87e00dfc709b0221449
Sai Aman Pothuganti <saiamanpothuganti@gmail.com>
2024-12-29 11:38:27 +0530

    update dist subtree

---
+++ dist/client/assets/chunks/chunk-B6Aj7L_O.js
@@ -0,0 +1,1 @@
+import{e as n,r as o,o as r,c as s,t as a}from"./chunk-BHVpcwqr.js";const _=n({__name:"Counter",setup(c){const t=o({count:0});return(u,e)=>(r(),s("button",{onClick:e[0]||(e[0]=p=>t.count++)},"Counter "+a(t.count),1))}});export{_};
---
+++ dist/server/entries/pages_markdown-page.mjs
@@ -0,0 +1,29 @@
+import { mergeProps, useSSRContext } from "vue";
+import { ssrRenderAttrs, ssrRenderComponent } from "vue/server-renderer";
+import { _ as _sfc_main$1 } from "../chunks/chunk-1ap2In98.js";
+const documentProps = {
+  title: "Some Markdown Page"
+};
+const _sfc_main = {
+  __name: "markdown.page",
+  __ssrInlineRender: true,
+  setup(__props, { expose: __expose }) {
+    const frontmatter = {};
+    __expose({ frontmatter });
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<div${ssrRenderAttrs(mergeProps({ class: "markdown-body" }, _attrs))}><h1>Markdown</h1><p>This page is written in <em>Markdown</em>.</p><p>Interactive components can be included in the Markdown. `);
+      _push(ssrRenderComponent(_sfc_main$1, null, null, _parent));
+      _push(`</p></div>`);
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/markdown.page.md");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as default,
+  documentProps
+};
